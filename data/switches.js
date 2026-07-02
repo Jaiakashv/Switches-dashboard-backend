89dfec573caa229b4d7d8308acf5248652006b2c
nova-service <Dukinikolic75@gmail.com>
2026-07-02 17:40:51 +0530

    first commit

---
+++ data/switches.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    model: 'Cisco Catalyst 9300',
+    physicalDevice: 'Rack A-01',
+    id: 'SW-1001',
+    config: 'Core routing + VLAN trunking',
+    status: 'Online',
+  },
+  {
+    model: 'Juniper EX3400',
+    physicalDevice: 'Rack B-02',
+    id: 'SW-1002',
+    config: 'Access layer with PoE',
+    status: 'Maintenance',
+  },
+  {
+    model: 'Netgear M4300',
+    physicalDevice: 'Lab Shelf 4',
+    id: 'SW-1003',
+    config: 'Test bench segmentation',
+    status: 'Offline',
+  },
+  {
+    model: 'Aruba CX 6200',
+    physicalDevice: 'Rack C-11',
+    id: 'SW-1004',
+    config: 'Campus distribution uplink',
+    status: 'Online',
+  },
+  {
+    model: 'HPE Aruba 2930F',
+    physicalDevice: 'Floor 2 Closet',
+    id: 'SW-1005',
+    config: 'Client access and voice VLANs',
+    status: 'Maintenance',
+  },
+  {
+    model: 'Dell N1524',
+    physicalDevice: 'Rack D-03',
+    id: 'SW-1006',
+    config: 'Lab aggregation switch',
+    status: 'Online',
+  },
+]
